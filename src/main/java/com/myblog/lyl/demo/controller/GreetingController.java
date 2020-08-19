a5497e3572b33d47a4029391bae2fa2a6775c694
lyl <wolylwo@gmail.com>
2020-08-19 12:16:00 +0800

    first repo

---
+++ src/main/java/com/myblog/lyl/demo/controller/GreetingController.java
@@ -0,0 +1,28 @@
+package com.myblog.lyl.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: demo
+ * @Date: 2020/8/19 10:42
+ * @Author: Yuling Li
+ * @Description:
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
